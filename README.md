1e0c550bc07470e4bdd75092f764b5ec0e44ae60
Tamas Majoros <tmsmajoros@gmail.com>
2019-09-27 18:23:16 +0200

    Added readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Markdown editor (Svelte.js training project)
+
+Very easy, very small Svelte.js project with marked.js. 
+
+[Click here](https://maja-md-editor.netlify.com)
--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   import marked from "marked";
-  $: mdown = `Some words are *italic*, some are **bold**`;
+  $: mdown = `# Publication
+
+Publishing in StackEdit makes it simple for you to publish online your files. Once you're happy with a file, you can publish it to different hosting platforms like **Blogger**, **Dropbox**, **Gist**, **GitHub**, **Google Drive**, **WordPress** and **Zendesk**. With [Handlebars templates](http://handlebarsjs.com/), you have full control over what you export.
+
+> Before starting to publish, you must link an account in the **Publish** sub-menu.`;
 </script>
 
 <style>
@@ -21,6 +25,10 @@
 
   textarea:focus {
     outline: none;
+  }
+
+  textarea {
+    margin-top: 1rem;
   }
 
   section {
